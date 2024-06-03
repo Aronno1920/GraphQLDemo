7eda27fc905308d8d6789452385415bc42b0ba86
Selim Ahmed <aronno1920@hotmail.com>
2024-06-04 02:27:51 +0600

    Update Query
    Get All Course and Course by ID done

--- GraphQLDemo.API/Schema/Query.cs
+++ GraphQLDemo.API/Schema/Query.cs
@@ -4,30 +4,50 @@ namespace GraphQLDemo.API.Schema
 {
     public class Query
     {
-        public IEnumerable<CourseType> GetCourcess()
+        private readonly Faker<InstructorType> instructorFaker;
+        private readonly Faker<StudentType> studentFacker;
+        private readonly Faker<CourseType> courseFaker;
+
+        public Query()
         {
-            Faker<InstructorType> instructorFaker = new Faker<InstructorType>()
+            instructorFaker = new Faker<InstructorType>()
                 .RuleFor(c => c.Id, d => Guid.NewGuid())
                 .RuleFor(c => c.FirstName, d => d.Name.FindName())
                 .RuleFor(c => c.LastName, d => d.Name.LastName())
                 .RuleFor(c => c.Salary, d => d.Random.Double(0, 1000000));
 
-            Faker<StudentType> studentFacker = new Faker<StudentType>()
+            studentFacker = new Faker<StudentType>()
                 .RuleFor(c => c.Id, d => Guid.NewGuid())
                 .RuleFor(c => c.FirstName, d => d.Name.FindName())
                 .RuleFor(c => c.LastName, d => d.Name.LastName())
                 .RuleFor(c => c.GPA, d => d.Random.Double(1, 4));
 
+            courseFaker = new Faker<CourseType>()
+                .RuleFor(c => c.Id, d => Guid.NewGuid())
+                .RuleFor(c => c.Name, f => f.Name.JobTitle())
+                .RuleFor(c => c.Subject, f => f.PickRandom<Subject>())
+                .RuleFor(c => c.Instructor, f => instructorFaker.Generate())
+                .RuleFor(c => c.Students, f => studentFacker.Generate(10));
+        }
+        
 
-            Faker<CourseType> courseFaker = new Faker<CourseType>();
-            courseFaker.RuleFor(c => c.Id, d => Guid.NewGuid());
-            courseFaker.RuleFor(c => c.Name, f => f.Name.JobTitle());
-            courseFaker.RuleFor(c => c.Subject, f => f.PickRandom<Subject>());
-            courseFaker.RuleFor(c => c.Instructor, f => instructorFaker.Generate());
-
+        public IEnumerable<CourseType> GetCourses()
+        {
             List<CourseType> courses = courseFaker.Generate(5);
+
             return courses;
         }
+
+        public CourseType GetCourseById(Guid Id)
+        {
+            CourseType course = courseFaker.Generate();
+            course.Id = Id;
+
+            return course;
+        }
+
+
+
 
         [GraphQLDeprecated("The message is deprecated")]
         public String welcomenote => "Welcome to GraphQL Demo project";
