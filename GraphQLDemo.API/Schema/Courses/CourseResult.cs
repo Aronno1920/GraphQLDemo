26c7bbac68fb51ec66c595b2a9f2346f32e51fdc
Selim Ahmed <aronno1920@hotmail.com>
2024-06-04 02:57:26 +0600

    Create CourseResult.cs

---
+++ GraphQLDemo.API/Schema/Courses/CourseResult.cs
@@ -0,0 +1,10 @@
+﻿namespace GraphQLDemo.API.Schema.Courses
+{
+    public class CourseResult
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public Guid InstructorId { get; set; }
+    }
+}
