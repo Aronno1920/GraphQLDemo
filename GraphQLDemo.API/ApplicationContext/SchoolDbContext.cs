5c5247f8f6423266f3fac4c662d5ccc2c737194a
Selim Ahmed <aronno1920@hotmail.com>
2024-06-09 01:25:20 +0600

    Update DbContext related

--- GraphQLDemo.API/ApplicationContext/SchoolDbContext.cs
+++ GraphQLDemo.API/ApplicationContext/SchoolDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace GraphQLDemo.API.ApplicationContext
+﻿using GraphQLDemo.API.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQLDemo.API.ApplicationContext
 {
-    public class SchoolDbContext
+    public class SchoolDbContext : DbContext
     {
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options):base(options) { }
+
+        public DbSet<CourseDTO> Courses { get; set; }
+        public DbSet<InstructorDTO> Instructors { get; set; }
+        public DbSet<StudentDTO> Students { get; set; }
     }
 }
--- GraphQLDemo.API/Program.cs
+++ GraphQLDemo.API/Program.cs
@@ -1,20 +1,19 @@
+using GraphQLDemo.API.ApplicationContext;
 using GraphQLDemo.API.Schema.Courses.Mutation;
 using GraphQLDemo.API.Schema.Courses.Query;
 using GraphQLDemo.API.Schema.Courses.Subscription;
+using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
+    .AddPooledDbContextFactory<SchoolDbContext>(d => d.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")))
     .AddGraphQLServer()
     .AddQueryType<CourseQuery>()
     .AddMutationType<CourseMutation>()
     .AddSubscriptionType<CourseSubscription>()
     .AddInMemorySubscriptions();
-
-
-
-
 
 var app = builder.Build();
 
