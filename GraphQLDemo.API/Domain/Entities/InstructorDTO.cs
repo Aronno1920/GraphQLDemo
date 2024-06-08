eb19a8f93f96a44b1f6c0fbf00359337d2e01762
Selim Ahmed <aronno1920@hotmail.com>
2024-06-09 01:31:05 +0600

    update models

--- GraphQLDemo.API/Domain/Entities/InstructorDTO.cs
+++ GraphQLDemo.API/Domain/Entities/InstructorDTO.cs
@@ -6,5 +6,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public double Salary { get; set; }
+
+        public IEnumerable<CourseDTO> Courses { get; set; }
     }
 }
